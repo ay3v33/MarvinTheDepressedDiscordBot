7303abef045f925bc4b680afc56af8f073e3cb5f
Aidan Van Voorhis <apvanvoorhis@gmail.com>
2023-10-09 21:00:15 -0700

    updates

--- index.js
+++ index.js
@@ -14,22 +14,9 @@ const client = new Client({
 
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
-  });
+});
 
 client.commands = new Collection();
-
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	if ('data' in command && 'execute' in command) {
-		client.commands.set(command.data.name, command);
-	} else {
-		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-	}
-}
 
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
