3c48b8f7e8573966644284fecf63b23c7224eb76
Jordan Weinstein <jordantw128@gmail.com>
2023-10-19 23:42:22 -0400

    syncdb.js

--- syncdb.js
+++ syncdb.js
@@ -1,4 +1,5 @@
-const Guild = require('./models/fish');
+const Guild = require('./models/guild');
+const Fish = require('./models/fish');
 
 //Guild.sync({force: true}); //resets database
 //Guild.sync({alter: true}); //updates database
